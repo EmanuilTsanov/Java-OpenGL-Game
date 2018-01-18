500ff43966f86adf9469b995d698950a4eb02e4a
Emanuil <Emanuil@DESKTOP-OG2G1E3>
2018-01-18 09:20:46 +0200

    Filtering implemented

--- Java-OpenGL-Game/src/opengl/java/management/FileManager.java
+++ Java-OpenGL-Game/src/opengl/java/management/FileManager.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
+import org.lwjgl.opengl.EXTTextureFilterAnisotropic;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL14;
 import org.lwjgl.opengl.GL30;
@@ -161,11 +162,15 @@ public class FileManager
 			tex = TextureLoader.getTexture("PNG", new FileInputStream(FileSRC.TEXTURES_FOLDER + fileName + "." + FileSRC.TEXTURE_EXTENSION));
 			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
 			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
-			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -0.5f);
-			if(GLContext.getCapabilities().GL_EXT_texture_filter_anisotropic) {
-				
-			} else {
-				
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, 0);
+			if (GLContext.getCapabilities().GL_EXT_texture_filter_anisotropic)
+			{
+				float amount = Math.min(4f, GL11.glGetFloat(EXTTextureFilterAnisotropic.GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT));
+				GL11.glTexParameterf(GL11.GL_TEXTURE_2D, EXTTextureFilterAnisotropic.GL_TEXTURE_MAX_ANISOTROPY_EXT, amount);
+			}
+			else
+			{
+
 			}
 		}
 		catch (IOException e)
--- Java-OpenGL-Game/src/opengl/java/window/Window.java
+++ Java-OpenGL-Game/src/opengl/java/window/Window.java
@@ -1,8 +1,12 @@
 package opengl.java.window;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.PixelFormat;
 
 public class Window
 {
@@ -10,6 +14,8 @@ public class Window
 
 	private int width = 1600;
 	private int height = 900;
+
+	private ContextAttribs attribs = new ContextAttribs(3, 3).withForwardCompatible(true).withProfileCore(true);
 
 	private WindowFrameController wfc = new WindowFrameController();
 	private static Window singleton = new Window();
@@ -20,7 +26,8 @@ public class Window
 		{
 			Display.setDisplayMode(new DisplayMode(width, height));
 			Display.setTitle(title);
-			Display.create();
+			Display.create(new PixelFormat().withSamples(8), attribs);
+			GL11.glEnable(GL13.GL_MULTISAMPLE);
 		}
 		catch (LWJGLException e)
 		{
