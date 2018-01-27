5f41c9921f08ab9cf029aeb2857617a45fcf2bb3
Emanuil <Emanuil@DESKTOP-OG2G1E3>
2018-01-27 10:25:45 +0200

    asdawsd

--- Java-OpenGL-Game/src/opengl/java/management/EntityManager.java
+++ Java-OpenGL-Game/src/opengl/java/management/EntityManager.java
@@ -32,10 +32,8 @@ public class EntityManager
 
 	public void loadEntities()
 	{
-		addEntities(Entity.pineTree, 100, false);
+		addEntities(Entity.pineTree, 1000, false);
 		addEntities(Entity.bench, 100, false);
-		addEntities(Entity.christmasTree, 100, false);
-		addEntities(Entity.snowman, 100, false);
 		addEntities(Entity.table, 10, false);
 		addEntities(Entity.campfire, 25, false);
 		addEntities(Entity.grass, 100, false);
