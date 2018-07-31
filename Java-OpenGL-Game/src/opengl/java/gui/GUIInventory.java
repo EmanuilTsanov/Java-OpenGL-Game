4f100f7c64b962a261eb828db6419da73ff9b2b2
Emanuil <Emanuil@DESKTOP-DBDM4RU>
2018-07-31 14:59:24 +0300

    aa

--- Java-OpenGL-Game/src/opengl/java/gui/GUIInventory.java
+++ Java-OpenGL-Game/src/opengl/java/gui/GUIInventory.java
@@ -7,6 +7,14 @@ import opengl.java.shader.GUIShader;
 public class GUIInventory
 {
 	private GUIWindow invWindow = new GUIWindow(0, 0, Display.getWidth() / 3, Display.getHeight());
+	private GUIItemList list = new GUIItemList((int) invWindow.getX() + invWindow.getWidth() / 10,
+			(int) invWindow.getY() + invWindow.getHeight() / 10, (int) (invWindow.getWidth() * 0.9f),
+			(int) (invWindow.getHeight() * 0.9f), 4);
+
+	public GUIInventory() {
+		invWindow.addChild(list);
+		list.addChild("").addChild("");
+	}
 
 	public void update()
 	{
--- Java-OpenGL-Game/src/opengl/java/gui/GUIItemList.java
+++ Java-OpenGL-Game/src/opengl/java/gui/GUIItemList.java
@@ -2,6 +2,7 @@ package opengl.java.gui;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import opengl.java.shader.GUIShader;
 
@@ -16,9 +17,10 @@ public class GUIItemList extends GUIComponent
 
 	private int currentPage = 0;
 
-	public GUIItemList(int x, int y, int width, int height)
+	public GUIItemList(int x, int y, int width, int height, int gridWidth)
 	{
 		super(x, y, width, height);
+		this.gridWidth = gridWidth;
 		this.itemWinWidth = (width - ((gridWidth + 1) * gridSpace)) / gridWidth;
 		itemPages = new HashMap<Integer, ArrayList<GUIItemWindow>>();
 	}
@@ -31,15 +33,16 @@ public class GUIItemList extends GUIComponent
 
 	public GUIItemList addChild(String image)
 	{
-		int x1 = (itemPages.get(currentPage).size() % gridWidth);
+		if (itemPages.get(currentPage) == null)
+			itemPages.put(currentPage, new ArrayList<GUIItemWindow>());
+		ArrayList<GUIItemWindow> arr = itemPages.get(currentPage);
+		int x1 = (arr.size() % gridWidth);
 		int x2 = x1 * itemWinWidth + (x1 + 1) * gridSpace;
-		int y1 = (int) (itemPages.get(currentPage).size() / gridWidth);
+		int y1 = (int) (arr.size() / gridWidth);
 		int y2 = y1 * itemWinWidth + (y1 + 1) * gridSpace;
 		if (y2 + itemWinWidth < y + height)
 		{
 			GUIItemWindow item = new GUIItemWindow(x2, y2, itemWinWidth, itemWinWidth).setImage(image);
-			if (itemPages.get(currentPage) == null)
-				itemPages.put(currentPage, new ArrayList<GUIItemWindow>());
 			itemPages.get(currentPage).add(item);
 		}
 		else
@@ -50,10 +53,24 @@ public class GUIItemList extends GUIComponent
 	@Override
 	public void update()
 	{
+		for (Map.Entry<Integer, ArrayList<GUIItemWindow>> entry : itemPages.entrySet())
+		{
+			for (GUIItemWindow g : entry.getValue())
+			{
+				g.update();
+			}
+		}
 	}
 
 	@Override
 	public void render(GUIShader shader)
 	{
+		for (Map.Entry<Integer, ArrayList<GUIItemWindow>> entry : itemPages.entrySet())
+		{
+			for (GUIItemWindow g : entry.getValue())
+			{
+				g.render(shader);
+			}
+		}
 	}
 }
