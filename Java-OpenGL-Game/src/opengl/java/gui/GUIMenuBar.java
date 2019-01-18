2a8295bb07fcd5aedaf9e2657114277b6988f51d
Emanuil <Emanuil@gs.apple.com>
2019-01-18 13:09:56 +0200

    a

--- Java-OpenGL-Game/src/opengl/java/gui/GUIMenuBar.java
+++ Java-OpenGL-Game/src/opengl/java/gui/GUIMenuBar.java
@@ -6,10 +6,16 @@ import opengl.java.shader.GUIShader;
 
 public class GUIMenuBar extends GUIComponent
 {
+	public int rowWidth;
 	public int buttonSize;
 	public int border;
-	
+
 	private ArrayList<GUIButton> buttons = new ArrayList<GUIButton>();
+
+	public GUIMenuBar(int rowWidth)
+	{
+		this.rowWidth = rowWidth;
+	}
 
 	public void addButton(GUIButton button)
 	{
