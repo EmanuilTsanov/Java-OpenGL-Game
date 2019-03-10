7d008accfc500f9298ceb5de84e4f65a1ceb96da
Emanuil <Emanuil@DESKTOP-K0ND10>
2019-03-10 23:39:21 +0200

    aaa

--- Java-OpenGL-Game/src/opengl/java/lighting/LightMaster.java
+++ Java-OpenGL-Game/src/opengl/java/lighting/LightMaster.java
@@ -8,7 +8,7 @@ import org.lwjgl.util.vector.Vector3f;
 public class LightMaster
 {
 	public static Light SUN = new Light(new Vector3f(3000, 2000, 2000), new Vector3f(0.4f, 0.4f, 0.4f));
-	public static Light l1 = new Light(new Vector3f(20, 5, 20), new Vector3f(10, 0,0), new Vector3f(1, 0.01f, 0.002f));
+	public static Light l1 = new Light(new Vector3f(20, 5, 20), new Vector3f(1,1,1), new Vector3f(1, 0.01f, 0.002f));
 	public static Light l2 = new Light(new Vector3f(1000, 50, 1000), new Vector3f(0,0, 10), new Vector3f(1, 0.01f, 0.002f));
 
 	public static List<Light> lights = new ArrayList<Light>();
